e8dc36feeb6daff4e6241a6ade3cbffbd1d9c813
Oleksandra-Rud <sandyrud.com@gmail.com>
2022-11-14 11:05:02 +0200

    fix: apdate js

--- app.js
+++ app.js
@@ -4,8 +4,33 @@ async function getBook() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      showBooks(data);
     });
 }
 
 getBook();
+
+function showBooks(respData) {
+  const booksEl = document.querySelector(".books");
+  respData.forEach((book) => {
+    const bookEl = document.createElement("div");
+    bookEl.classList.add("book");
+    bookEl.innerHTML = `<div class="book__cover-inner">
+            <img
+              src="${book.imageLink}"
+              class="book__cover"
+              style="width: 300px"
+            />
+            <div class="book__cover--darkened"></div>
+          </div>
+
+          <div class="book__info">
+            <div class="book__title">${book.title}</div>
+            <div class="book__author">${book.author}</div>
+            <div class="book__price"><span class="priceN">${book.price}</span> $</div>
+            <div class="book__button">
+              <button type="button" class="add-to-bag">Add to Bag</button>
+            </div>`;
+    booksEl.appendChild(bookEl);
+  });
+}
